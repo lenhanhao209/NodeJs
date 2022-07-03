b471b7d97942ce36a66c3d5c5e827c3527a70868
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-03 23:06:28 +0700

    Lab1-6

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const handlebars = require("express-handlebars");
 const app = express();
 const fs = require("fs");
 
+app.use(express.urlencoded({ extended: true }));
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "/views"));
@@ -13,9 +14,16 @@ app.get("/", (req, res) => {
 });
 
 app.post("/message", (req, res) => {
-  fs.writeFileSync("message.txt", "DUMMY");
-  res.redirect("/");
-  res.statusCode = 302;
+  const message = req.body.message;
+  try {
+    fs.writeFileSync("message.txt", message);
+    res.status(302);
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    res.status(401);
+  }
+  res.end();
 });
 
 app.listen(3000);
