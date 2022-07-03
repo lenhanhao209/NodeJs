ba7dfec7ba4f38a81f08452647060f39870c4581
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-03 12:20:36 +0700

    Lab1-4

--- first-app.js
+++ first-app.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const path = require("path");
+const handlebars = require("express-handlebars");
 const app = express();
 
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "resources/views"));
+
 app.get("/", (req, res) => {
-  return res.send("Hello from my Node.js server!");
+  return res.render("home");
+});
+
+app.get("/message", (req, res) => {
+  return res.render();
 });
 
 app.listen(3000);
