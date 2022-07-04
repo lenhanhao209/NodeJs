062f82b7af9bfb1d5abf8cc30cce192702b6406e
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-04 23:42:34 +0700

    Thêm trang xử lý lỗi 404

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(adminRoutes);
-app.use(shopRoutes);
+
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "/views"));
 
+app.use(adminRoutes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 app.listen(3000);
