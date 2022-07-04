35ed7647711bd3284367d200d5c000e2935d9d9e
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-04 15:44:40 +0700

    Cài đặt Middleware

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+
+app.use((req, res, next) => {
+  console.log("This is the first middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("This is the second middleware");
+  next();
+});
+
 const server = http.createServer(app);
 server.listen(3000);
