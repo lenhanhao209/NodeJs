091b8e1f4af12a6ddf314f149e0c10a1c0c4a368
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-05 19:19:10 +0700

    Trả lại trang 404

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const handlebars = require("express-handlebars");
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./views/routes/admin");
+const shopRoutes = require("./views/routes/shop");
+
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "/views"));
@@ -14,6 +15,6 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 app.listen(3000);
