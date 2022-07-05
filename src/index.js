71df7b7d7d6af00ebc26f2dd295c4e14d94e24bc
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-05 22:00:59 +0700

    Test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const rootDir = require("./views/util/path");
 const handlebars = require("express-handlebars");
 const adminRoutes = require("./views/routes/admin");
 const shopRoutes = require("./views/routes/shop");
 
+app.use(express.static(path.join(__dirname, "/public")));
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
-app.set("views", path.join(rootDir, "/views"));
+app.set("views", path.join(__dirname, "/views"));
 
 app.use(express.urlencoded({ extended: true }));
 
