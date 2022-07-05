3d74355ddce28f3703e41498a0c7099e781eeca3
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-05 17:06:55 +0700

    Cung cấp các trang HTML

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const handlebars = require("express-handlebars");
-const adminRoutes = require("./views/routes/admin");
-const shopRoutes = require("./views/routes/shop");
-
-app.use(express.urlencoded({ extended: true }));
-
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "/views"));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
