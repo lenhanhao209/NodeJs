bd3b05a2354e3962f09c3eb5fae4f6a7f87ea6ab
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-06 11:55:18 +0700

    Cài đặt & triển khai Pug

--- src/index.js
+++ src/index.js
@@ -12,9 +12,8 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "./views"));
 
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/admin", adminData.routes);
 app.use(shopRoutes);
+app.use("/admin", adminData.routes);
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
