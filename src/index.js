b8a3147909da414528604c4bd42ff165e3067515
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-07 16:39:54 +0700

    Chuyển đổi tệp HTML sang Pug

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ app.use(shopRoutes);
 app.use("/admin", adminData.routes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.render("404");
 });
 app.listen(3000);
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -7,12 +7,6 @@ router.get("/add-product", (req, res, next) => {
   res.render("add-product");
 });
 
-// router.post("/product", (req, res, next) => {
-//   res.render("product");
-//   console.log(adminData.products);
-//   products.push({ title: req.body.title });
-// });
-
 router.post("/product", (req, res, next) => {
   products.push({ title: req.body.title });
   res.redirect("/");
