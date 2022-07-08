e8d7ef05595338ba38a5cb70ef10a436c225410c
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-08 22:41:13 +0700

    Thêm bố cục vào Handlebars

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -10,7 +10,7 @@ router.get("/add-product", (req, res, next) => {
   });
 });
 
-router.post("/admin/product", (req, res, next) => {
+router.post("/product", (req, res, next) => {
   products.push({ title: req.body.title });
   console.log(products);
   res.redirect("/");
