d1fc4ff568ea9a2d6e792ec4878acfe78da1c68d
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-08 22:30:53 +0700

    Test

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -10,8 +10,9 @@ router.get("/add-product", (req, res, next) => {
   });
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/admin/product", (req, res, next) => {
   products.push({ title: req.body.title });
+  console.log(products);
   res.redirect("/");
 });
 exports.products = products;
