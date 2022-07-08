f7ce6d643268f564b00952cdf420f4404a42792a
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-08 21:25:59 +0700

    Chuyển đổi ứng dụng sang Handlebars

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const shopRoutes = require("./routes/shop");
 
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "./views"));
-
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "/views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(shopRoutes);
 app.use("/admin", adminData.routes);
