7afd556f3dafe623acb87a41c6b83d283e1d1c4b
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-09 10:17:19 +0700

    Hoàn thiện Controller

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const handlebars = require("express-handlebars");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorsController = require("./controllers/errors");
 
 app.use(express.static(path.join(__dirname, "/public")));
 
@@ -15,7 +16,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(shopRoutes);
 app.use("/admin", adminRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "PAGE NOT FOUND!!!" });
-});
+app.use(errorsController.get404Page);
 app.listen(3000);
