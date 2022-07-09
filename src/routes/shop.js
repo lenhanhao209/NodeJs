d2bf7b45cac5937f872d26768c7a2a5bff973b7f
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-09 09:29:22 +0700

    Bố cục với Partials

--- src/routes/shop.js
+++ src/routes/shop.js
@@ -6,7 +6,6 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   const products = adminData.products;
   res.render("home", { prods: products, pageTitle: "Shop", path: "/" });
-  console.log(products[0].title);
 });
 
 module.exports = router;
