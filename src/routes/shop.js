77e60344170573f947baa775bf11b51374417525
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-11 08:57:36 +0700

    Thêm ID sản phẩm vào đường dẫn

--- src/routes/shop.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const adminData = require("./admin");
-const productsControllers = require("../controllers/products");
-const router = express.Router();
-
-router.get("/", productsControllers.getHome);
-
-module.exports = router;
