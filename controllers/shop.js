dcc6f888d0d79c5e05f5409a060098758579d5a0
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-16 10:39:18 +0700

    Thực hành Tìm nạp sản phẩm

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,9 +2,9 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
+  Product.fetchAll().then(([rows, fieldData]) => {
     res.render("shop/product-list", {
-      prods: products,
+      prods: rows,
       pageTitle: "All Products",
       path: "/products",
     });
