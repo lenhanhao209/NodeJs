c7697a4edf7d943a2c1e5aab33f602eb3001e400
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-17 00:00:45 +0700

    Đồng bộ hóa các định nghĩa JS với Cơ sở dữ liệu

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-
+const sequelize = require("./util/database");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -21,4 +21,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
