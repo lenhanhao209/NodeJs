e13317b5e21185bec2abf0934d2bf8a849b16b9b
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-07-19 22:45:54 +0700

    Thêm các sản phẩm hiện có & lấy các mặt hàng trong giỏ hàng

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -10,10 +17,6 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const User = require("./models/user");
-const Product = require("./models/product");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -40,17 +43,20 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
+  // .sync({ force: true })
   .sync()
   .then((result) => {
     return User.findByPk(1);
+    // console.log(result);
   })
   .then((user) => {
     if (!user) {
-      return User.create({ name: "Max", email: "test@gmail.com" });
+      return User.create({ name: "Max", email: "test@test.com" });
     }
     return user;
   })
   .then((user) => {
+    // console.log(user);
     return user.createCart();
   })
   .then((cart) => {
--- controllers/admin.js
+++ controllers/admin.js
@@ -21,6 +21,8 @@ exports.postAddProduct = (req, res, next) => {
       description: description,
     })
     .then((result) => {
+      // console.log(result);
+      console.log("Created Product");
       res.redirect("/admin/products");
     })
     .catch((err) => {
@@ -58,17 +60,16 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-
   Product.findByPk(prodId)
     .then((product) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
-      product.imageUrl = updatedImageUrl;
-      updatedTitle;
       product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
       return product.save();
     })
     .then((result) => {
+      console.log("UPDATED PRODUCT!");
       res.redirect("/admin/products");
     })
     .catch((err) => console.log(err));
@@ -84,9 +85,7 @@ exports.getProducts = (req, res, next) => {
         path: "/admin/products",
       });
     })
-    .catch((err) => {
-      console.log(err);
-    });
+    .catch((err) => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
@@ -96,9 +95,8 @@ exports.postDeleteProduct = (req, res, next) => {
       return product.destroy();
     })
     .then((result) => {
+      console.log("DESTROYED PRODUCT");
       res.redirect("/admin/products");
     })
-    .catch((err) => {
-      console.log(err);
-    });
+    .catch((err) => console.log(err));
 };
