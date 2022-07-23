ed14ce577de952d1e363241b5e6cfc65edc27d85
lenhanhao209 <100815614+lenhanhao209@users.noreply.github.com>
2022-07-23 21:17:37 +0700

    Sửa chức năng ' Thêm sản phẩm'

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ class Product {
         this.price = price;
         this.description = description;
         this.imageUrl = imageUrl;
-        this._id = new mongodb.ObjectId(id);
+        this._id = id ? new mongodb.ObjectId(id) : null;
     }
 
     save() {
@@ -15,9 +15,7 @@ class Product {
         let dbOp;
         if (this._id) {
             // Update the product
-            dbOp = db
-                .collection("products")
-                .updateOne({ _id: this._id }, { $set: this });
+            dbOp = db.collection("products").updateOne({ _id: this._id }, { $set: this });
         } else {
             dbOp = db.collection("products").insertOne(this);
         }
