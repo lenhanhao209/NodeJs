7df3b9d3a00b753fcf3ef777e90b05287890a7aa
lenhanhao209 <100815614+lenhanhao209@users.noreply.github.com>
2022-07-24 17:05:37 +0700

    Làm việc trên các mặt hàng & đơn đặt hàng trong giỏ hàng

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const mongoConnect =
-  require("./util/database").mongoConnect;
+const mongoConnect = require("./util/database").mongoConnect;
 const User = require("./models/user");
 
 const app = express();
