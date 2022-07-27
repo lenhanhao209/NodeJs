5befc75de05714bb1c477dc7c4e20863bd5bed5d
lenhanhao209 <100815614+lenhanhao209@users.noreply.github.com>
2022-07-27 11:14:14 +0700

    Nhận & Hiển thị các Đơn đặt hàng

--- controllers/shop.js
+++ controllers/shop.js
@@ -108,15 +108,16 @@ exports.postOrder = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-// exports.getOrders = (req, res, next) => {
-//   req.user
-//     .getOrders()
-//     .then((orders) => {
-//       res.render("shop/orders", {
-//         path: "/orders",
-//         pageTitle: "Your Orders",
-//         orders: orders,
-//       });
-//     })
-//     .catch((err) => console.log(err));
-// };
+exports.getOrders = (req, res, next) => {
+  Order.find({ "user.userId": req.user._id })
+    .then((orders) => {
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
