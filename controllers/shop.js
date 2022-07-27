bb15fccc16aab18e6b23d2c955e933e6664f644d
lenhanhao209 <100815614+lenhanhao209@users.noreply.github.com>
2022-07-27 09:25:02 +0700

    Tạo & Nhận đơn hàng

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -79,24 +80,42 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .addOrder()
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((item) => {
+        return {
+          quantity: item.quantity,
+          product: item.productId,
+        };
+      });
+
+      const order = new Order({
+        users: {
+          name: req.user.name,
+          userId: req.user,
+        },
+        products: products,
+      });
+
+      return order.save();
+    })
+
     .then((result) => {
       res.redirect("/orders");
     })
     .catch((err) => console.log(err));
 };
 
-exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
-    .then((orders) => {
-      res.render("shop/orders", {
-        path: "/orders",
-        pageTitle: "Your Orders",
-        orders: orders,
-      });
-    })
-    .catch((err) => console.log(err));
-};
+// exports.getOrders = (req, res, next) => {
+//   req.user
+//     .getOrders()
+//     .then((orders) => {
+//       res.render("shop/orders", {
+//         path: "/orders",
+//         pageTitle: "Your Orders",
+//         orders: orders,
+//       });
+//     })
+//     .catch((err) => console.log(err));
+// };
