7ca2e3727b15bab5d6d740e9fc6d9604611ad85a
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-08-08 20:58:56 +0700

    Lab8-7

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,4 @@
 exports.getLogin = (req, res, next) => {
-  // const isLoggedIn = req.get("Cookie").split("=")[1] === "true";
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
