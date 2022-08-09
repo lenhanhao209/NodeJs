3cf54b39fd7a3d79a94a3b15f1afdfe1482dc4fd
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-08-09 10:32:33 +0700

    Lab8-12

--- app.js
+++ app.js
@@ -57,18 +57,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "lenhanhao",
-          email: "tranlenhanhao1990@gmail.com",
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch((err) => {
