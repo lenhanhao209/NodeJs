2bb6cf43cf68f505b70ed758222b2cbe5a530a3c
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-08-11 16:51:08 +0700

    Lab9-4

--- routes/auth.js
+++ routes/auth.js
@@ -1,38 +1,46 @@
-const express = require('express');
-const { check } = require('express-validator/check');
+const express = require("express");
+const { check, body } = require("express-validator/check");
 
-const authController = require('../controllers/auth');
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin);
+router.get("/login", authController.getLogin);
 
-router.get('/signup', authController.getSignup);
+router.get("/signup", authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post("/login", authController.postLogin);
 
 router.post(
-  '/signup',
-  check('email')
-    .isEmail()
-    .withMessage('Please enter a valid email.')
-    .custom((value, { req }) => {
-      if (value === 'test@test.com') {
-        throw new Error('This email address if forbidden.');
-      }
-      return true;
-    }),
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((value, { req }) => {
+        if (value === "test@test.com") {
+          throw new Error("This email address if forbidden.");
+        }
+        return true;
+      }),
+    body(
+      "password",
+      "Password must be only contain number and text at more 6 characters"
+    )
+      .isLength({ min: 6 })
+      .isAlphanumeric(),
+  ],
   authController.postSignup
 );
 
-router.post('/logout', authController.postLogout);
+router.post("/logout", authController.postLogout);
 
-router.get('/reset', authController.getReset);
+router.get("/reset", authController.getReset);
 
-router.post('/reset', authController.postReset);
+router.post("/reset", authController.postReset);
 
-router.get('/reset/:token', authController.getNewPassword);
+router.get("/reset/:token", authController.getNewPassword);
 
-router.post('/new-password', authController.postNewPassword);
+router.post("/new-password", authController.postNewPassword);
 
 module.exports = router;
