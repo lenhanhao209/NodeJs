a5ec4c44bc159c39dbefd4362b2f93c7077a0fa9
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-08-14 14:43:15 +0700

    Lab10-1

--- app.js
+++ app.js
@@ -71,7 +71,6 @@ app.use(authRoutes);
 app.get("/500", errorController.get500);
 app.use(errorController.get404);
 app.use((error, req, res, next) => {
-  // res.redirect("/500");
   res.status(404).render("500", {
     pageTitle: "Error!!",
     path: "/500",
