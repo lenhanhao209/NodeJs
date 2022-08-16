70f6a6646d2d9676c30bd372812dd73378b7f0e5
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-08-16 09:31:30 +0700

    Lab10-17

--- controllers/admin.js
+++ controllers/admin.js
@@ -39,7 +39,6 @@ exports.postAddProduct = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    console.log(errors.array());
     return res.status(422).render("admin/edit-product", {
       pageTitle: "Add Product",
       path: "/admin/add-product",
