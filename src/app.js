77555111cc10da7b4974d1118865f2a4f3a10dab
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-08-22 23:58:31 +0700

    ASM2

--- src/app.js
+++ src/app.js
@@ -126,4 +126,6 @@ app.use((req, res, next) => {
 // Init router
 router(app);
 
-app.listen(3000);
+app.listen(process.execArgv.PORT || 3001, "0.0.0.0", () => {
+  console.log("Server is running");
+});
